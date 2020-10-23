62eea294febb96bec7ae5e1414b192c70e1d9964
Kemp <32265362+kempspo@users.noreply.github.com>
2020-10-23 18:17:05 +0800

    pull from pelican conf

--- publishconf.py
+++ publishconf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from pelicanconf import *
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
@@ -11,7 +12,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://www.kemppo.com'
+# SITEURL = 'https://www.kemppo.com'
+SITEURL = 'https://0.0.0.0'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
